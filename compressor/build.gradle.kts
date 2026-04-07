f228931dae4a2efd1e71d460b6a60061c615ff48
vulturelan <lanjianwei1992@gmail.com>
2026-04-07 16:39:42 +0800

    update version to 1.0.2

--- compressor/build.gradle.kts
+++ compressor/build.gradle.kts
@@ -18,7 +18,7 @@ afterEvaluate {
 
                 groupId = "com.github.vulturelan"
                 artifactId = "compressor"
-                version = "1.0.0"
+                version = "1.0.2"
             }
         }
     }
